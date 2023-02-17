8126fabf6fd9abb2e1aa9bf2422b11b49b2627aa
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-17 23:18:53 +0100

    fix: update Token and navigation route

--- src/components/Protected.tsx
+++ src/components/Protected.tsx
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { protectedType } from "../logic/types";
 
-const Protected = () => {
-  const auth = { token: true };
-  return auth.token ? <Outlet /> : <Navigate to="/" />;
+const Protected = ({ Token }: protectedType) => {
+  return Token ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default Protected;
