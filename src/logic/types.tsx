4317b630caa5a185dd69d8e09e464374a49a65c0
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-17 15:46:24 +0100

    feat: create `types` file

---
+++ src/logic/types.tsx
@@ -0,0 +1,27 @@
+// SIGN UP
+export type initialSignUpType = {
+  name: string;
+  family: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+  address: string;
+  phoneNumber: string;
+  passportNumber: string;
+};
+
+export type signUpAction = {
+  type: string;
+  payload: string;
+};
+
+// LOGIN
+export type initialLoginType = {
+  username: string;
+  password: string;
+};
+
+export type LoginAction = {
+  type: string;
+  payload: string;
+};
