257a51d3ba04a36a0da372dd271c5495d6d3d7bc
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-17 23:26:20 +0100

    feat: add routes and authToken

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Protected from "./components/Protected";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Home from "./Home";
+import { useState } from "react";
+import { authTokenType } from "./logic/types";
 
 function App() {
-  return <div className="App"></div>;
+  const [authToken, setAuthToken] = useState<authTokenType>({
+    Token: null,
+    Username: null,
+  });
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Protected Token={authToken.Token} />}>
+          <Route
+            path="/"
+            element={
+              <Home
+                authToken={authToken}
+                signOut={() => setAuthToken({ Token: null, Username: null })}
+              />
+            }
+          />
+        </Route>
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/login"
+          element={
+            <Login
+              setAuthToken={(Token, Username) => {
+                setAuthToken({ Token, Username });
+              }}
+            />
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
