dd169dbf212511337f67728de876db707ebb0dbd
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-18 18:59:29 +0100

    feat: create axiosUserRequest

--- src/logic/requests.tsx
+++ src/logic/requests.tsx
@@ -1,7 +1,33 @@
 import axios, { AxiosRequestConfig } from "axios";
-import { axiosParameter } from "./types";
+import { axiosParameter, axiosUserParameter } from "./types";
 
 export const axiosRequest = async ({ url, method, data }: axiosParameter) => {
+  const config: AxiosRequestConfig = {
+    url,
+    method,
+    data,
+  };
+
+  return await axios(config)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return {
+        status: false,
+        message: {
+          token: null,
+          description: err.message,
+        },
+      };
+    });
+};
+
+export const axiosUserRequest = async ({
+  url,
+  method,
+  data,
+}: axiosUserParameter) => {
   const config: AxiosRequestConfig = {
     url,
     method,
