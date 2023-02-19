0d360b3e10023adabdf165ee6370f704bc05725b
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-19 09:48:34 +0100

    feat: create validation functions

---
+++ src/logic/validation.tsx
@@ -0,0 +1,24 @@
+import { initialSignUpType } from "./types";
+
+export const notEmpty = (data: initialSignUpType) => {
+  return !Object.values(data).includes("");
+};
+
+export const matchPasswords = (data: initialSignUpType) => {
+  return data.password == data.repeatPassword;
+};
+
+export const validatePhoneNumber = (data: initialSignUpType) => {
+  const first =
+    data.phoneNumber.slice(0, 1) == "+" ? "" : data.phoneNumber.slice(0, 1);
+  const second = data.phoneNumber.slice(1);
+  const fullNumber = `${first}${second}`;
+  return parseInt(fullNumber).toString() == fullNumber;
+};
+
+export const validatePassportNumber = (data: initialSignUpType) => {
+  const largerThanZero = parseInt(data.passportNumber) > 0;
+  const isANumber =
+    parseInt(data.passportNumber).toString() == data.passportNumber;
+  return largerThanZero && isANumber;
+};
