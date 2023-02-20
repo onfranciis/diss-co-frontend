c4c525db05638a75d7e472f93e6caa7454e46408
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-20 10:38:35 +0100

    fix: add Spanish to language

--- src/translations/translations.tsx
+++ src/translations/translations.tsx
@@ -1,9 +1,16 @@
 import { createContext, useContext } from "react";
 import { TranslationContextType, translationFileType } from "../logic/types";
-import { english } from "./english";
+import english from "./english.json";
+import spanish from "./spanish.json";
 
 export const changeTranslationFile = (data: string): translationFileType => {
   switch (data) {
+    case "English":
+      return english;
+
+    case "Spanish":
+      return spanish;
+
     default:
       return english;
   }
